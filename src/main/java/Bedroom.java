33cb13c3cb5039e879ca8d02f5b6f99646b7a75b
ghr85 <ghrenfrew@gmail.com>
2019-03-27 11:27:58 +0000

    added room number functionality to bedroom model class

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+    private Integer roomNumber;
+    private Integer capacity;
+    private ArrayList<Guest> guests;
+    private String bedroomType;
+
+    public Bedroom(Integer roomNumber, Integer capacity, String bedroomType){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+        this.bedroomType = bedroomType;
+    }
+    public int getRoomNumber() {
+        return this.roomNumber;
+    }
 }
--- src/test/java/TestBedroom.java
+++ src/test/java/TestBedroom.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestBedroom {
+    Bedroom bedroom;
+    Guest guest;
+    @Before
+    public void before(){
+        bedroom = new Bedroom(1,2,"double");
+        guest = new Guest("Davie Dipstick");
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(1,bedroom.getRoomNumber());
+    }
 }
